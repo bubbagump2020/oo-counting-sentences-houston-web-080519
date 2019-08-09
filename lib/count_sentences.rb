44695b6f197eaf1f3624c69bd8065a0e49c1704a
Kevin Bagnall <bubbagump2020@gmail.com>
2019-08-09 14:44:53 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -21,8 +21,8 @@ class String
     complex_sentence_array = []
     
     
-    sentence_array = (self.split("."))
-    binding.pry 
+    sentence_array = (self.split(".")).to_i
+    #binding.pry 
     sentence_array.count
     
     
